9559c0b4aba190d5ea23a500885ca82021c30c00
Yonatan-Ethiopia <yonatanethiopia1@gmail.com>
2026-08-22 09:40:27 +0300

    Add error.js and auth code

---
+++ job_board_platform/src/errors/AppError.ts
@@ -0,0 +1,24 @@
+export class AppError extends Error {
+    constructor(public statusCode: number, message: string){
+        super(message);
+        Object.setPrototypeOf(this, new.target.prototype);
+    }
+}
+
+export class NotFoundError extends AppError {
+    constructor( message = 'Resource not found'){
+        super(404, message);
+    }
+}
+
+export class BadRequestError extends AppError {
+    constructor( message = 'Bad request'){
+        super(400, message);
+    }
+}
+
+export class ConflictError extends AppError {
+    constructor( message = "Resource already exists"){
+        super(409, message)
+    }
+}
--- job_board_platform/src/modules/auth/auth.service.ts
+++ job_board_platform/src/modules/auth/auth.service.ts
@@ -1,1 +1,21 @@
-async function registerUser(input)
+import express, { Request, Response} from "express";
+import prisma from "./prisma/client.js";
+import { ConflictError } from "./errors/AppError.js";
+
+async function registerUser(input: { name: string; email: string; password: string; role: RoleEnum}){
+
+    try {
+        const emailExists = await prisma.user.findUnique({ where: { email:input. email});
+        if (emailExists){
+
+            throw new ConflictError("Email already exists");
+                
+        }
+
+        const hashedPassword = await bcrypt.hash(input.password, 10);
+
+        
+
+    }    
+
+}
